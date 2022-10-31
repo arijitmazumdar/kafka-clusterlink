df153c21c00ca8605efc5212197e1f5f2cf0c376
Arijit Mazumdar <arijit.mazumdar@gmail.com>
2022-10-31 23:20:40 +0530

    adding stop scripts to cleanup

--- cleanup.sh
+++ cleanup.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+echo stopping kafka servers..
+
+kafka-server-stop
+
+echo stopping zookeeper servers..
+zookeeper-server-stop
+
+echo cleaning data directories..
 rm -rf /tmp/zookeeper-1
 rm -rf /tmp/kafka-logs-1
 rm -rf /tmp/zookeeper-2
